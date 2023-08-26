f42817b031f283df084284c3dbe0050269744cc0
AhmetBozkurt1 <ahmet_bozkurt812@outlook.com>
2023-08-26 18:21:41 +0300

    hata düzeltildi

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ let ac=document.querySelector("#ac")
 //*tekrar bir if çevirmem gerekli çünkü ben AC butonuna basınca alanda 0 gözüksün number butonuna bastığımda da 0 giderek sayılar üzerine eklenmye devam etsin diyorum
 //*o yüzden tekrar bir if döndürmek zorundayım calText içerisinde 0 varsa onu siliyorum ve altta tıklanan elemanları üzerine eklerek yazdırıyorum
 
+let sayi1;
+let operation;
+
 document.querySelector(".calcNumber").addEventListener("click",function(e){
     if(e.target.className.includes("number")){
 //*burada if döndürmeyip direkt calcText="" yazsaydım sürekli içeriyi boşaltıp number ekleyecekti sayılar yan yana gelmeyecekti
@@ -16,12 +19,30 @@ document.querySelector(".calcNumber").addEventListener("click",function(e){
         calcText.value+=e.target.innerHTML;
         ac.innerHTML="C"
     }
+    if(e.target.id.includes("bölüm")){
+        sayi1=calcText.value
+        operation="bölüm"
+        calcText.value=""
+    }
+
+    if(e.target.id.includes("eşit")){
+        if(operation==="bölüm"){
+            let sayi2=calcText.value
+            if (sayi2 !== 0) {
+                calcText.value = sayi1 / sayi2;
+            } else {
+                calcText.value = "Hata!";
+            }
+        }
+    }
 });
+
 //*AC butonuna tıklanıldığında alandakı tüm numaralar sıfırladım yani direkt sıfır değeri verdim ve içeriği C olan butonun içeriğni AC yaptım
 ac.addEventListener("click",function(){
     calcText.value=0
     $("#ac").text("AC")
 })
+
 
 
 
